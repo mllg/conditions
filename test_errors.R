aee23b32ca899ee041ed80803ccd10a55faa29f4
Michel Lang <michellang@gmail.com>
2016-07-08 16:03:51 +0200

    cleanup

--- test_errors.R
+++
@@ -1,9 +0,0 @@
-context("errors")
-
-test_that("errors", {
-  expect_error(stop(assertion_error()), "Generic")
-  expect_error(stop(assertion_error("foo")), "foo")
-
-  expect_equal(class(assertion_error("foo")), c("assertion_error", "error", "condition"))
-  expect_equal(class(index_error()), c("index_error", "error", "condition"))
-})
